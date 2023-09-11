357613f5157d573d7cb69ae2f4fe12c800882f78
Daniel Tehrani <contact@dantehrani.com>
2023-09-11 13:37:34 +0900

    chore: expose mock_circuit so it can be used in benchmarking

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(non_snake_case)]
 mod constraint_system;
-#[cfg(test)]
 mod test_utils;
 
 // Exports and re-exports
@@ -10,4 +9,5 @@ pub use constraint_system::{CircuitMeta, ConstraintSystem, Wire};
 pub use shockwave_plus::ark_ff;
 pub use shockwave_plus::ark_secp256k1;
 pub use shockwave_plus::FieldGC;
+pub use test_utils::mock_circuit;
 pub use wasm::wasm_deps;
